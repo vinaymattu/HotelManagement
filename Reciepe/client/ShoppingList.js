5fddecb496b82663c835acd15e8aed81dc88edbc
Vinay Mattu <vmattu9@gmail.com>
2016-03-09 01:19:17 +0530

    created the shopping list page

---
+++ Reciepe/client/ShoppingList.js
@@ -0,0 +1,14 @@
+Template.ShoppingList.onCreated(function() {
+	var self = this;
+	self.autorun(function() {
+		self.subscribe('recipes');
+	});
+});
+
+
+
+Template.ShoppingList.helpers({
+	shoppingList: ()=> {
+		return Recipes.find({inMenu: true});
+	}
+});
