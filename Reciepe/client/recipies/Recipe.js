429f4b94a6a620b53ad9d947eb277e40ab951ccd
Vinay Mattu <vmattu9@gmail.com>
2016-03-11 02:23:37 +0530

    delete from menu added and update menu fixed

--- Reciepe/client/recipies/Recipe.js
+++ Reciepe/client/recipies/Recipe.js
@@ -9,5 +9,8 @@ Template.Recipe.helpers({
 Template.Recipe.events({
 	'click.toggle-menu':function(){
 		Meteor.call('toggleMenuItem',this._id,this.inMenu);
+	},
+	'click .fa-trash' : function(){
+		Meteor.call('deleteRecipe',this._id);
 	}
 });
